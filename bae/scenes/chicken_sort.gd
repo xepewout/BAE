9386f9422915d5e1f62560c1ef86c2bc37aeb2cf
xepewout <111144938+xepewout@users.noreply.github.com>
2025-02-06 16:13:21 -0800

    version .8.2
    -chicken sort minigame

--- bae/scenes/chicken_sort.gd
+++ bae/scenes/chicken_sort.gd
@@ -2,9 +2,24 @@ extends Node2D
 
 var totalPairs = 2
 var pairsFound = 0
+var flag = 0
+var data = 0
+var Tempchicken: Area2D
 
-func _click():
-	pass
+func _click() -> bool:
+	print(data)
+	if data == 0:
+		print("data is 0")
+		pairsFound += 1
+		return true
+		_checkIfDone()
+	data = 0
+	return false
 
-func _ready():
-	pass
+func _checkIfDone():
+	if pairsFound == totalPairs:
+		print("done")
+		
+func _setChicken(chicken: Area2D):
+	Tempchicken = chicken
+	
---
+++ bae/scenes/fox_1.gd
@@ -0,0 +1,21 @@
+extends Area2D
+@onready var chicken_sort: Node2D = $".."
+@onready var chicken: Area2D = $"."
+
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("mouse_left"):
+		chicken_sort.flag += 1
+		chicken_sort.data -= 2
+		chicken.visible = false
+		if chicken_sort.flag == 2  or chicken_sort.flag == 0:
+			chicken_sort.flag = 0
+			if !chicken_sort._click():
+				chicken.visible = true
+				chicken_sort.Tempchicken.visible = true
+				chicken_sort.Tempchicken = null
+				print("click was false")
+		else:
+			print("setChicken")
+			chicken_sort._setChicken(chicken)
